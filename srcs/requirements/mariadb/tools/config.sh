0b40816d725d293081f7c5dcb7a62343ade3a641
guphilip <guphilip@guphilip.myguest.virtualbox.org>
2025-07-28 13:38:31 +0200

    + | MariaDB container init

---
+++ srcs/requirements/mariadb/tools/config.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
+rm -f /run/mysqld/mysqld.sock
+
+# INIT MARIADB IF NECESSARY
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "[INFO] Initializing MariaDB data directory..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+    mysqld --user=mysql --skip-networking &
+    pid="$!"
+
+    # Wait for mariabd
+    until mysqladmin ping --silent; do 
+        echo "[INFO] Waiting for MariaDB to be ready..."
+        sleep 1
+    done
+
+    echo "[INFO] Creating database and users..."
+    mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS \`${MDB_NAME}\`;
+CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PWD}';
+CREATE USER IF NOT EXISTS '${MDB_USER}'@'localhost' IDENTIFIED BY '${MDB_PWD}';
+GRANT ALL PRIVILEGES ON \`${MDB_NAME}\`.* TO '${MDB_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${MDB_NAME}\`.* TO '${MDB_USER}'@'localhost';
+ALTER USE 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT}';
+FLUSH PRIVILEGES;
+EOF
+
+    mysqladmin -u root -p"${MDB_ROOT}" shutdown || kill "$pid"
+fi
+
+echo "[INFO] Starting MariaDB..."
+exec mysqld --user=mysql --bind-address=0.0.0.0 --port=3306
